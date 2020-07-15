208b12e30af2cfe5507aa78d868638b4d37683ed
Ria <riaparishseo@gmail.com>
2020-07-15 21:22:28 +1200

    Env stuff

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,8 +1,14 @@
+from __future__ import absolute_import, unicode_literals
 from .base import *
+
+import os
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['13.238.219.1', 'riaparish.co.nz'] 
+ALLOWED_HOSTS = ['13.238.219.1', 'riaparish.co.nz']
+
+env = os.environ.copy()
+SECRET_KEY = env['SECRET_KEY']
 
 try:
     from .local import *
